f7e3b56b857a04b01a4aa4a3571af00abaf20b82
k.afanasev <afanasev.kirill@gmail.com>
2021-02-22 15:08:27 +0300

    Add Get & Delete Posts routs & tests.

--- api/src/routes/api/posts.js
+++ api/src/routes/api/posts.js
@@ -37,4 +37,65 @@ router.post('/',
 
     })
 
+// @router GET api/posts
+// @desc   Get all posts
+// @access Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({date: -1})
+        return res.send(posts)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @router GET api/posts/:id
+// @desc   Get post by ID
+// @access Private
+// TODO При передаче параметра :1 система возвращает Пост, ожидаемое поведение 404.
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+
+        if (!post) {
+            return res.status(404).json({msg: 'Post not found'})
+        }
+
+        return res.send(post)
+    } catch (err) {
+        console.error(err.message)
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({msg: 'Post not found'})
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
+// @router DELETE api/posts/:id
+// @desc   Delete post by ID
+// @access Private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+
+        if (!post) {
+            return res.status(404).json({msg: 'Post not found'})
+        }
+        //Check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({msg: 'User is not authorized'})
+        }
+
+        await post.remove()
+        return res.json({msg: 'Post removed'})
+    } catch (err) {
+        console.error(err.message)
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({msg: 'Post not found'})
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router;
