7514df8691265783fa810cb7a325fd2f29f0bc2d
k.afanasev <afanasev.kirill@gmail.com>
2021-02-28 12:25:05 +0300

    Add Profile routes&types&actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,17 @@ import {Provider} from 'react-redux'
 
 import './App.css'
 import store from "./store";
+import {loadUser} from './actions/auth'
+
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Alert from './components/layout/Alert'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/routing/PrivateRoute'
 import setAuthToken from "./utils/setAuthToken";
-import {loadUser} from './actions/auth'
+
 
 if (localStorage.token) {
     setAuthToken(localStorage.token)
@@ -32,6 +36,7 @@ const App = () => {
                         <Switch>
                             <Route exact path="/login" component={Login}/>
                             <Route exact path="/register" component={Register}/>
+                            <PrivateRoute exact path="/dashboard" component={Dashboard}/>
                         </Switch>
                     </section>
                 </Fragment>
