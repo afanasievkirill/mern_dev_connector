7761c1cc37fe6f02c660697121756b91a2b3f64e
k.afanasev <afanasev.kirill@gmail.com>
2021-04-04 14:30:53 +0300

    Add Prometheus

--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,15 @@ const {connectDb} = require("./helpers/db");
 const {port, db} = require("./configuration");
 const app = require('./app')
 
+const client = require('prom-client')
+const collectDefaultMetrics = client.collectDefaultMetrics
+collectDefaultMetrics({ prefix: 'api:3001' })
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.send(await client.register.metrics());
+})
+
 const startServer = () => {
     app.listen(port, () => {
         console.log(`Started api service on port: ${port}`);
--- frontend/src/reducers/profile.js
+++ frontend/src/reducers/profile.js
@@ -1,8 +1,10 @@
 import {
     GET_PROFILE,
+    GET_REPOS,
     UPDATE_PROFILE,
     PROFILE_ERROR,
     CLEAR_PROFILE,
+    GET_PROFILES,
 } from '../actions/types'
 
 const initialState = {
@@ -24,6 +26,12 @@ function profileReducer(state = initialState, action) {
                 profile: payload,
                 loading: false
             }
+        case GET_PROFILES:
+            return {
+                ...state,
+                profiles: payload,
+                loading: false
+            }
         case PROFILE_ERROR:
             return {
                 ...state,
@@ -35,6 +43,13 @@ function profileReducer(state = initialState, action) {
                 ...state,
                 profile: null,
                 repos: [],
+                loading: false
+            }
+        }
+        case GET_REPOS: {
+            return {
+                ...state,
+                repos: payload,
                 loading: false
             }
         }
